d4ddf8eb85236e3990354b102f83b91eb50f4327
Judah Wright <hello@judahwright.me>
2020-06-17 12:11:25 -0700

    Adding ability to fetch a movie given its IMDB id

--- app/Movie.php
+++ app/Movie.php
@@ -6,6 +6,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Facades\Http;
 
 final class Movie extends Model
 {
@@ -23,6 +24,21 @@ final class Movie extends Model
         'rating',
         'keywords',
     ];
+
+    public static function fetchFromImdb(string $imdbId): self
+    {
+        $imdbInfo = Http::get(sprintf('https://imdb-api.com/en/API/Title/%s/%s', config('services.imdb.key'), $imdbId))->object();
+        return new self([
+            'imdb_id' => $imdbId,
+            'title' => $imdbInfo->fullTitle,
+            'image_url' => $imdbInfo->image,
+            'runtime' => $imdbInfo->runtimeStr,
+            'plot' => $imdbInfo->plot,
+            'genres' => $imdbInfo->genres,
+            'rating' => $imdbInfo->contentRating,
+            'keywords' => implode(', ', array_map('ucwords', $imdbInfo->keywordList))
+        ]);
+    }
 
     public function polls(): BelongsToMany
     {
