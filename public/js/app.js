5242fbe4b3077bc6801362060021bcd4bb7d15c1
EpistemicPolymath <epistemicpolymath@gmail.com>
2020-09-09 19:04:58 -0400

    Just getting started doing more design research

--- public/js/app.js
+++ public/js/app.js
@@ -49627,8 +49627,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/judah/Documents/Work/Movie Night Planner/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/judah/Documents/Work/Movie Night Planner/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! T:\Work\Personal Projects\Movie Night Planner\Movie-Night-Planner\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! T:\Work\Personal Projects\Movie Night Planner\Movie-Night-Planner\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,6 +13,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
