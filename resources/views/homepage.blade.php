f434171985c47220ac7ad7fb43d981f6ce60d66b
Judah Wright <hello@judahwright.me>
2020-09-09 18:08:27 -0700

    Adding some routes

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,33 +5,29 @@
 
        <nav>
         <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">Login</a></li>
-            <li><a href="">Register</a></li>
-            <li><a href="">About Us</a></li>
-            <li><a href="">FAQ</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="{{ route('login') }}">Login</a></li>
+            <li><a href="{{ route('register') }}">Register</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">FAQ</a></li>
         </ul>
         </nav>
 
         <header>
         <h1><span>Movie</span><span>Night</span><span>Planner</span></h1>
         <a href="">
-            <img src="{{ url('images/corina-rainer-popcorn-unsplash.jpg') }}" alt="popcorn">
+            <img src="{{ asset('images/corina-rainer-popcorn-unsplash.jpg') }}" alt="popcorn">
         </a>
 
         </header>
 
-    
         <section>
         <!-- Where info about the website goal will be -->
-
 
         </section>
 
         <footer>
 
-
         </footer>
     </div>
-    
 @endsection
